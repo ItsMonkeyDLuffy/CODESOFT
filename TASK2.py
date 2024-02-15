e285b57fee4ee7da51ab38585822ae6acd4e4318
ItsMonkeyDLuffy <gupta.vaibhav9313@gmail.com>
2024-02-15 22:52:21 +0530

    Task 1 completed

---
+++ TASK2.py
@@ -0,0 +1,18 @@
+a = int(input("Write first no."))
+b = int(input("Write second no."))
+c = input("What do you want to do: +, -, *, / \n")
+
+if c == "+":
+    print(a+b)
+elif c == "-":
+    print(a-b)
+elif c == "*":
+    print(a*b)
+elif c == "/":
+    print(a/b)
+else:
+    print("Invalid Input")
+
+
+
+
