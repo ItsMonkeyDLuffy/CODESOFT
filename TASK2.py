055cd7f447cad9cbcf7f89818811b006692e7372
ItsMonkeyDLuffy <gupta.vaibhav9313@gmail.com>
2024-02-18 11:24:02 +0530

    Task 4 completed

--- TASK2.py
+++ TASK2.py
@@ -1,5 +1,5 @@
-a = int(input("Write first no."))
-b = int(input("Write second no."))
+a = int(input("Write first no.:"))
+b = int(input("Write second no.:"))
 c = input("What do you want to do: +, -, *, / \n")
 
 if c == "+":
---
+++ TASK4.py
@@ -0,0 +1,29 @@
+
+import random
+
+while True:
+    a = input("stone, paper or scissors: ").lower()
+    
+    if a in ["stone", "paper", "scissors"]:
+        b = random.choice(["stone", "paper", "scissors"])
+        print("Computer chooses:", b)
+        if a == b:
+            print("Draw")
+        elif (a == "stone" and b == "scissors") or \
+            (a == "paper" and b == "stone") or \
+            (a == "scissors" and b == "paper"):
+            print("User wins")
+        else:
+            print("Computer wins")
+    else:
+        print("Invalid choice. Please enter 'stone', 'paper', or 'scissors'.")
+        continue
+
+    c = input("Do you want to play again (yes/no): ")
+    if c.lower() != "yes":
+        print("Thank you for playing")
+        break
+    
+    
+
+    
